86e63830416cfd982928c74d07193d906bb21354
Jachin-Khoo <jachinkhoo@gmail.com>
2026-09-07 09:19:02 +0800

    fa

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -2,11 +2,36 @@
     import { ref } from 'vue'
 
     // Add code here
+    /* method 1 */
+    import me from '/assets/me.png'
+
+    const obj = {
+        src: me,
+        width: "x",
+        height: "y"
+    }
+
+    /* method 2 */
+    const width=ref("200")
+    const height=ref("200")    
+    const myImg = ref('/assets/me.png')
+
+    /* method 3 */
+
+    const img = {
+        src: '/assets/me.png',
+        width:"200",
+        height:"200"
+    }
 
 </script>
 
 <template>
 
     <!-- Add code here -->
-   
+    <img v-bind:src="myImg" :width="width" :height="height"/>
+
+    <img v-bind="obj" />
+
+
 </template>
