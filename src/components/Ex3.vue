866d585940aa7bd9344e7f52a60cdb28440edfa5
Jachin-Khoo <jachinkhoo@gmail.com>
2026-09-07 09:39:58 +0800

    EWe

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,23 +1,27 @@
 <script setup>
-    import { ref } from 'vue'
+import { ref } from 'vue'
 
-    // Add code here
-    
-    
+// Add code here
+const login = ref('username')
 </script>
 
 <template>
-    <h1>Please select your login option</h1>
-    <!-- Add/modify code in the following to have a dropdown list 
+  <h1>Please select your login option</h1>
+  <!-- Add/modify code in the following to have a dropdown list 
         which lets the user to select either to login with username or email -->
-    <select>
-        <option selected>Username login</option>
-        <option>Email login</option>
-    </select>
-    
-    <div>
-        <label>Username</label>
-        <input placeholder="Enter your username">
-    </div>
-</template>
+  <select name="login" v-model="login">
+    <option value="username">Username login</option>
+    <option value="email">Email login</option>
+  </select>
 
+  <div>
+    <template v-if="login == 'username'">
+      <label>Username</label>
+      <input placeholder="Enter your username" />
+    </template>
+    <template v-else="login == 'email'">
+      <label>Email</label>
+      <input placeholder="Enter your email address" />
+    </template>
+  </div>
+</template>
